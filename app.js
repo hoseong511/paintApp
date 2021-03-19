5b0661338966b313b94a48c7228a8f9c2a18a928
hoseong511 <thdghtjd115@naver.com>
2021-03-19 17:40:44 +0900

    update css 컬러버튼 효과, js 브러쉬크기, :

--- app.js
+++ app.js
@@ -1,35 +1,80 @@
 const canvas = document.getElementById("jsCanvas");
+const colors = document.querySelectorAll(".jsColor");
 const ctx = canvas.getContext("2d");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 
+canvas.width = 700;
+canvas.height = 700;
 ctx.strokeStyle="##2c2c2c";
 ctx.lineWidth = 2.5;
+
+
 let painting = false;
+let filling = false;
 
-
+const SHOWING_CN = "showing";
 
 function stopPainting(){
   painting=false;
 }
 
-function startPainting(event){
+function startPainting(){
   painting=true;
 }
 
 function onMouseMove(event){
   const x = event.offsetX;
   const y = event.offsetY;  
-  
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else{
+    ctx.lineTo(x, y);    
+    ctx.stroke();
+  }  
 }
 
-function onMouseDown(event){
-  console.log(event);
-  painting = true;
-
+function handleColorClick(event){
+  colors.forEach(color =>
+    color.classList.remove(SHOWING_CN));
+  const color = event.target.style.backgroundColor;  
+  event.target.classList.add(SHOWING_CN)
+  ctx.strokeStyle = color;
 }
 
-if(canvas && click){  
+function handlRangeChage(event){
+  const size = event.target.value
+  ctx.lineWidth = size;
+  console.log(event.target.value);
+}
+
+function handleModeClick(){
+  if(filling === true){
+    filling = false;
+    mode.innerText = "Fill";
+  } else{
+    filling = true;
+    mode.innerText="Paint";
+  }
+}
+
+if(canvas){  
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+}
+
+if(colors){
+  Array.from(colors).forEach(color =>  
+    color.addEventListener("click", handleColorClick));
+}
+
+if(range){
+  range.addEventListener("input", handlRangeChage);
+}
+
+if(mode){
+  mode.addEventListener("click",handleModeClick)
 }
