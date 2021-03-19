af2fefa09f27cedcebfeb1d0aa16c1d40277e135
hoseong511 <thdghtjd115@naver.com>
2021-03-19 14:56:23 +0900

    update canvas events

---
+++ app.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("jsCanvas");
+let painting = false;
+let click = true;
+
+
+function stopPainting(){
+  painting=false
+}
+
+function onMouseMove(event){
+  const x = event.offsetX;
+  const y = event.offsetY;  
+  console.log(x, y);  
+}
+
+function onMouseDown(event){
+  console.log(event);
+  painting = true;
+
+}
+function onMouseUp(event){
+  stopPainting();
+}
+
+
+
+if(canvas && click){  
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
